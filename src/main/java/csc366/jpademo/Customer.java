16e3206233f61f583d65afcce1b3455c1978b903
Matthew Wingerden <matthew.wingerden@gmail.com>
2024-02-28 18:00:55 -0800

    Added relationship with person class.

--- src/main/java/csc366/jpademo/Customer.java
+++ src/main/java/csc366/jpademo/Customer.java
@@ -1,4 +1,29 @@
 package csc366.jpademo;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.StringJoiner;
+
+import javax.persistence.*;
+
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(
+        name = "Customer"
+)
 public class Customer {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long customerID;
+
+    @Column(name="RewardsOption")
+    private boolean rewardsOption;
+
+    @Column(name="PaymentType")
+    private String paymentType;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "customerID", referencedColumnName = "ID")
+    private Person person;
 }
