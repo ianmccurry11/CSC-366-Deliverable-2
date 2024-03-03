9bcc2ccc83bca02644c242792fae18704626b99b
Matthew Wingerden <matthew.wingerden@gmail.com>
2024-03-02 16:40:34 -0800

    Finished my version of customer and customer order.

--- src/main/java/csc366/jpademo/Customer.java
+++ src/main/java/csc366/jpademo/Customer.java
@@ -19,11 +19,12 @@ import javax.persistence.OneToMany;
 
 @Entity
 @Table(name = "Customer")
-public class Customer extends Person {
+public class Customer {
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name = "customerID", referencedColumnName = "ID")
-    private Person person;
+    @Id
+    @Column(name = "CustomerID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerID;
 
     @Column(name = "RewardsOption")
     private boolean rewardsOption;
@@ -31,16 +32,29 @@ public class Customer extends Person {
     @Column(name = "PaymentType")
     private String paymentType;
 
+    @OneToOne(fetch = FetchType.LAZY, mappedBy = "customer")
+    @JoinColumn(name = "CustomerID", referencedColumnName = "ID")
+    private Person person;
+
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CustomerOrder> customerOrders = new ArrayList<>();
 
     public Customer() {
     }
 
-    public Customer(Long customerID, boolean rewardsOption, String paymentType) {
-//        super(customerID);
+    public Customer(boolean rewardsOption, String paymentType, Person person) {
         this.rewardsOption = rewardsOption;
         this.paymentType = paymentType;
+        this.person = person;
+        this.customerID = person.getId();
+    }
+
+    public Long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Long customerID) {
+        this.customerID = customerID;
     }
 
     public boolean isRewardsOption() {
@@ -59,6 +73,14 @@ public class Customer extends Person {
         this.paymentType = paymentType;
     }
 
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
     public List<CustomerOrder> getCustomerOrders() {
         return customerOrders;
     }
@@ -70,7 +92,7 @@ public class Customer extends Person {
     @Override
     public String toString() {
         StringJoiner sj = new StringJoiner(",", Customer.class.getSimpleName() + "[", "]");
-        sj.add(super.toString()).add(String.valueOf(this.rewardsOption)).add(this.paymentType);
+        sj.add(this.customerID.toString()).add(String.valueOf(this.rewardsOption)).add(this.paymentType);
         return sj.toString();
     }
 
@@ -79,17 +101,13 @@ public class Customer extends Person {
         if (this == o) return true;
         if (!(o instanceof Customer)) return false;
         Customer customer = (Customer) o;
-        return true;
-//        return isRewardsOption() == customer.isRewardsOption() &&
-//                Objects.equals(getID(),
-//                        customer.getID()) &&
-//                Objects.equals(getPaymentType(), customer.getPaymentType())
-//                && Objects.equals(person, customer.person);
+        return isRewardsOption() == customer.isRewardsOption() && Objects.equals(getCustomerID(),
+                customer.getCustomerID()) && Objects.equals(getPaymentType(), customer.getPaymentType())
+                && Objects.equals(person, customer.person);
     }
 
     @Override
     public int hashCode() {
-        return 0;
-//        return Objects.hash(getID(), isRewardsOption(), getPaymentType(), person);
+        return Objects.hash(getCustomerID(), isRewardsOption(), getPaymentType(), person);
     }
 }
