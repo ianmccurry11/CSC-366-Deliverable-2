529fed3ea9108f558946edc00a27d57c55f28565
ianmccurry11 <ianmccurry4@gmail.com>
2024-03-08 16:25:27 -0800

    changed query varibales to fix errors

--- src/main/java/csc366/jpademo/SupplierRepository.java
+++ src/main/java/csc366/jpademo/SupplierRepository.java
@@ -19,15 +19,15 @@ public interface SupplierRepository extends JpaRepository<Supplier, Long> {
     Supplier findByNameJpql(@Param("name") String name);
 
     // JPQL query with join (assuming there is an 'addresses' field in the Customer entity)
-    @Query("select s from Supplier s join s.contracts ctr where c.name = :name")
+    @Query("select s from Supplier s join s.contracts ctr where s.name = :name")
     Supplier findByNameWithJpql(@Param("name") String name);
 
     // Native SQL query (validity not checked, invalid SQL will cause runtime exception)
-    @Query(value = "select * from supplier as s where c.name = :name ", nativeQuery = true)
+    @Query(value = "select * from supplier as s where s.name = :name ", nativeQuery = true)
     Supplier findByNameSql(@Param("name") String name);
 
     @Modifying
-    @Query("update Supplier s set s.name = :newName where c.name = :oldName")
+    @Query("update Supplier s set s.name = :newName where s.name = :oldName")
     void updateName(@Param("oldName") String oldName, @Param("newName") String newName);
 
     // Add more custom queries based on your requirements
